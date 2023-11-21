aa62a7a1ef4f7709a37e5ffc9d51da1fbf3f4b93
ronbonnke <ronbonnke789@gmail.com>
2023-11-21 11:38:24 +0530

    Your commit message here

--- src/Filter.js
+++ src/Filter.js
@@ -4,9 +4,9 @@ import "./Filter.css";
 function Filter() {
   return (
     <div className="filter d-flex align-items-center">
-      <div className="single-filter">
+      {/* <div className="single-filter">
         <span className="filter-items pointer active">All</span>
-      </div>
+      </div> */}
       <div className="single-filter">
         <span className="filter-items pointer ">Mixes</span>
       </div>
